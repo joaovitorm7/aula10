a21564da9e56b50f7f899cd8aebfaec600d2cc35
João Vitor Maia Ribeiro <joaomaia760@gmail.com>
2026-03-26 08:56:42 -0300

    feat: resolvendo o desafio 29 do curso de python mundo 1, aula 10, condições.

---
+++ desafio029.py
@@ -0,0 +1,9 @@
+##Exercício Python 29: Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+
+velocidade = float(input('Qual é a velocidade do carro? '))
+if velocidade > 80:
+    print('Você foi multado!')
+    multa = (velocidade - 80) * 7
+    print('O valor da multa é R${:.2f}' .format(multa))
+else:
+    print('Você está dentro do limite de velocidade.')
